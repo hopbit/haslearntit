fbbc38ea9b4aecb4230bec3736315d7a536c4d59
Krzysztof Jelski <kjelski@gmail.com>
2011-12-09 15:05:25 +0100

    server will stop after test

--- src/test/java/endtoend/AddEntryEndToEndTest.java
+++ src/test/java/endtoend/AddEntryEndToEndTest.java
@@ -33,8 +33,8 @@ public class AddEntryEndToEndTest extends IntegrationTest {
 
     @After
     public void shutdownSeleniumAndServer() throws Exception {
-        // driver.close();
-        server.join();
+        driver.close();
+        server.stop();
     }
 
     private static Server createServer() {
