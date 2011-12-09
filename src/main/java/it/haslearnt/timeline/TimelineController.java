c9e58367da0718cf7eda3a0f207a6cafc0a8afb0
Pawel Lipinski <lipinski.pawel@gmail.com>
2011-12-09 16:58:46 +0100

    mappings for user.jsp and login.jsp

--- src/main/java/it/haslearnt/timeline/TimelineController.java
+++ src/main/java/it/haslearnt/timeline/TimelineController.java
@@ -1,14 +1,24 @@
 package it.haslearnt.timeline;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class TimelineController {
 
-    @RequestMapping("/")
-    public String mainTimelineView() {
-        return "timeline";
-    }
+	@RequestMapping("/")
+	public String mainTimelineView() {
+		return "timeline";
+	}
+
+	@RequestMapping("/user")
+	public String userView() {
+		return "user";
+	}
+
+	@RequestMapping("/logout")
+	public String logoutView() {
+		return "login";
+	}
 
 }
