0df2237d3b3f6db4240789f707d64a7ef53e2b7f
Pawel Lipinski <lipinski.pawel@gmail.com>
2011-12-09 16:39:10 +0100

    test fix

--- src/test/java/endtoend/AddEntryEndToEndTest.java
+++ src/test/java/endtoend/AddEntryEndToEndTest.java
@@ -1,60 +1,57 @@
 package endtoend;
 
-import org.eclipse.jetty.server.Connector;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.bio.SocketConnector;
-import org.eclipse.jetty.webapp.WebAppContext;
-import org.junit.After;
-import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.eclipse.jetty.server.*;
+import org.eclipse.jetty.server.bio.*;
+import org.eclipse.jetty.webapp.*;
+import org.junit.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.*;
 
-import setup.IntegrationTest;
+import setup.*;
 
 public class AddEntryEndToEndTest extends IntegrationTest {
 
-    private static final int PORT = 6345;
-    private WebDriver driver;
-    private Server server;
+	private static final int PORT = 6345;
+	private WebDriver driver;
+	private Server server;
 
-    @Test
-    public void shouldAddEntry() throws Exception {
-        server = createServer();
-        addContextToServer(server);
-        server.start();
+	@Test
+	public void shouldAddEntry() throws Exception {
+		server = createServer();
+		addContextToServer(server);
+		server.start();
 
-        driver = new FirefoxDriver();
+		driver = new FirefoxDriver();
 
-        driver.navigate().to("http://localhost:" + PORT + "/");
+		driver.navigate().to("http://localhost:" + PORT + "/");
 
-        driver.findElement(By.id("submit-entry")).submit();
+		driver.findElement(By.id("entry")).submit();
 
-    }
+	}
 
-    @After
-    public void shutdownSeleniumAndServer() throws Exception {
-        driver.close();
-        server.stop();
-    }
+	@After
+	public void shutdownSeleniumAndServer() throws Exception {
+		driver.close();
+		server.stop();
+	}
 
-    private static Server createServer() {
-        Server server = new Server();
-        SocketConnector connector = new SocketConnector();
+	private static Server createServer() {
+		Server server = new Server();
+		SocketConnector connector = new SocketConnector();
 
-        // Set some timeout options to make debugging easier.
-        connector.setMaxIdleTime(1000 * 60 * 60);
-        connector.setSoLingerTime(-1);
-        connector.setPort(PORT);
-        server.setConnectors(new Connector[] { connector });
-        return server;
-    }
+		// Set some timeout options to make debugging easier.
+		connector.setMaxIdleTime(1000 * 60 * 60);
+		connector.setSoLingerTime(-1);
+		connector.setPort(PORT);
+		server.setConnectors(new Connector[] { connector });
+		return server;
+	}
 
-    private static void addContextToServer(Server server) {
-        WebAppContext webAppContext = new WebAppContext();
-        webAppContext.setServer(server);
-        webAppContext.setContextPath("/");
-        webAppContext.setWar("src/main/webapp");
-        server.setHandler(webAppContext);
-    }
+	private static void addContextToServer(Server server) {
+		WebAppContext webAppContext = new WebAppContext();
+		webAppContext.setServer(server);
+		webAppContext.setContextPath("/");
+		webAppContext.setWar("src/main/webapp");
+		server.setHandler(webAppContext);
+	}
 }
