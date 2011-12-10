de50ae9bb5eae0ed380c5c2094b6a6f856023c51
Kamil Berdychowski <k.berdychowski@gmail.com>
2011-12-10 11:46:11 +0100

    Fix

--- src/main/java/it/haslearnt/controller/StaticticsController.java
+++ src/main/java/it/haslearnt/controller/StaticticsController.java
@@ -1,26 +1,22 @@
 package it.haslearnt.controller;
 
 import it.haslearnt.service.UserStaticticsService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
 public class StaticticsController {
 
-	private UserStaticticsService userStaticticsService;
+    private UserStaticticsService userStaticticsService;
 
+    public StaticticsController(UserStaticticsService userStaticticsService) {
+        this.userStaticticsService = userStaticticsService;
+    }
 
-	public StaticticsController(UserStaticticsService userStaticticsService) {
-		this.userStaticticsService = userStaticticsService;
-	}
+    public ModelAndView getTotalLearningTime(String userName) {
 
-	public ModelAndView getTotalLearningTime(String userName) {
-
-		final ModelAndView modelAndView = new ModelAndView("user");
-		modelAndView.addObject("hours", userStaticticsService.getHoursForUser(userName));
-		return modelAndView;
-	}
-
+        final ModelAndView modelAndView = new ModelAndView("user");
+        modelAndView.addObject("hours", userStaticticsService.getHoursForUser(userName));
+        return modelAndView;
+    }
 
 }
