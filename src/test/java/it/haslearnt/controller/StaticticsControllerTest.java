1e166d24405089043556726f360429da8f9d3008
Kamil Berdychowski <k.berdychowski@gmail.com>
2011-12-10 12:05:27 +0100

    Removed autowire by constructor

--- src/test/java/it/haslearnt/controller/StaticticsControllerTest.java
+++ src/test/java/it/haslearnt/controller/StaticticsControllerTest.java
@@ -1,53 +1,59 @@
 package it.haslearnt.controller;
 
 import it.haslearnt.service.UserStaticticsService;
+
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.springframework.web.servlet.ModelAndView;
 
 public class StaticticsControllerTest {
 
-	private UserStaticticsService service = Mockito.mock(UserStaticticsService.class);
-	private StaticticsController controler = new StaticticsController(service);
+    private UserStaticticsService service = Mockito.mock(UserStaticticsService.class);
+    private StaticticsController controler = new StaticticsController();
 
+    @Before
+    public void setupServiceInController() {
+        controler.userStaticticsService = service;
+    }
 
-	@Test
-	public void shouldReturnViewName() throws Exception {
-		// given
-		String userName = null;
+    @Test
+    public void shouldReturnViewName() throws Exception {
+        // given
+        String userName = null;
 
-		// when
-		ModelAndView totalLearningTime = controler.getTotalLearningTime(userName);
+        // when
+        ModelAndView totalLearningTime = controler.getTotalLearningTime(userName);
 
-		// then
-		Assert.assertEquals("user", totalLearningTime.getViewName());
-	}
+        // then
+        Assert.assertEquals("user", totalLearningTime.getViewName());
+    }
 
-	@Test
-	public void shouldParametrExists() throws Exception {
-		// given
-		String userName = null;
+    @Test
+    public void shouldParametrExists() throws Exception {
+        // given
+        String userName = null;
 
-		// when
-		ModelAndView totalLearningTime = controler.getTotalLearningTime(userName);
+        // when
+        ModelAndView totalLearningTime = controler.getTotalLearningTime(userName);
 
-		// then
-		Assert.assertTrue(totalLearningTime.getModel().containsKey("hours"));
-	}
+        // then
+        Assert.assertTrue(totalLearningTime.getModel().containsKey("hours"));
+    }
 
-	@Test
-	public void shouldGetHoursFromService() {
-		// given
-		final String userName = "mariusz";
-		Mockito.when(service.getHoursForUser(userName)).thenReturn(5);
+    @Test
+    public void shouldGetHoursFromService() {
+        // given
+        final String userName = "mariusz";
+        Mockito.when(service.getHoursForUser(userName)).thenReturn(5);
 
-		// when
-		ModelAndView totalLearningTime = controler.getTotalLearningTime(userName);
+        // when
+        ModelAndView totalLearningTime = controler.getTotalLearningTime(userName);
 
-		// then
-		Assert.assertEquals(5, totalLearningTime.getModel().get("hours"));
-		Mockito.verify(service).getHoursForUser(userName);
-	}
+        // then
+        Assert.assertEquals(5, totalLearningTime.getModel().get("hours"));
+        Mockito.verify(service).getHoursForUser(userName);
+    }
 
 }
