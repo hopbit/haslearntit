33099ae51f3e67e542f175b6792577018f8b6cf4
Pawel Lipinski <lipinski.pawel@gmail.com>
2011-12-10 12:11:00 +0100

    Repo annotated

--- src/main/java/it/haslearnt/skill/trends/SkillTrendsRepository.java
+++ src/main/java/it/haslearnt/skill/trends/SkillTrendsRepository.java
@@ -4,6 +4,9 @@ import it.haslearnt.cassandra.mapping.*;
 
 import java.util.*;
 
+import org.springframework.stereotype.*;
+
+@Repository
 public class SkillTrendsRepository extends CassandraRepository<SkillTrend> {
 
 	public List<SkillTrend> loadTop(int i) {
